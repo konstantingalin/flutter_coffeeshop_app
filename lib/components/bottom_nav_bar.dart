41eb72ebc7961ca550c84808b543f856f3d6739b
Konstantin <the.it.promo@gmail.com>
2023-03-29 15:44:51 +0600

    add nav_bar and models

---
+++ lib/components/bottom_nav_bar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class MyBottomNavBar extends StatelessWidget {
+  MyBottomNavBar({super.key, required this.onTabChange});
+
+  void Function(int)? onTabChange;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(24),
+      child: GNav(
+        onTabChange:(value) => onTabChange!(value),
+        color: Colors.grey[400],
+        mainAxisAlignment: MainAxisAlignment.center,
+        activeColor: Colors.grey[700],
+        tabBackgroundColor: Colors.grey.shade300,
+        tabBorderRadius: 24,
+        tabActiveBorder: Border.all(color: Colors.white),
+        tabs: const [
+          GButton(
+            icon: Icons.home,
+            text: 'Shop',
+            margin: EdgeInsets.all(8),
+          ),
+          GButton(
+            icon: Icons.shopping_bag_outlined,
+            text: 'Cart',
+            margin: EdgeInsets.all(8),
+          ),
+        ]),
+    );
+  }
+}
---
+++ lib/models/coffe_shop.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_coffee_shop_app/models/coffee.dart';
+
+class CoffeeShop {
+
+  // coffee for sale list
+  final List<Coffee> _shop = [
+    Coffee(
+      name: 'Long Black',
+      price: '4.10',
+      imagePath: 'lib/images/black.png',
+    ),
+    Coffee(
+      name: 'Latte',
+      price: '4.20',
+      imagePath: 'lib/images/latte.png',
+    ),
+    Coffee(
+      name: 'Expresso',
+      price: '3.50',
+      imagePath: 'lib/images/expresso.png',
+    ),
+    Coffee(
+      name: 'Iced Coffee',
+      price: '4.40',
+      imagePath: 'lib/images/iced_coffee.png',
+    ),
+  ];
+
+  // user cart
+  List <Coffee> _userCart = [];
+
+  // get coffee list
+  List <Coffee> get coffeeShop => _shop;
+
+  // get user cart
+  List <Coffee> get userCart => _userCart;
+
+  // add item to cart
+  void addItemToCart(Coffee coffee){
+    _userCart.add(coffee);
+  }
+
+  // remove item from cart
+  void removeItemToCart(Coffee coffee){
+    _userCart.remove(coffee);
+  }
+}
